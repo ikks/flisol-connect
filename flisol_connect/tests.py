5240a4b28056a35df59c9599ace89e05a858f65c
Igor Támara <igor@tamarapatino.org>
2014-12-31 13:51:29 -0500

    Fixed tests

--- flisol_connect/tests.py
+++ flisol_connect/tests.py
@@ -1,8 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+from datetime import date
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from flisol_event.models import FlisolEvent
+
+from django_dynamic_fixture import G
+
 
 class GeneralTests(TestCase):
+
+    def setUp(self):
+        official_date = date.today() + timedelta(30)
+        G(
+            FlisolEvent,
+            official_date=official_date,
+        )
 
     def test_main_page(self):
         """Tests the visibility of the home"""
