8e242ed4433422bf75d7494cb11a05b47c4ad0fd
Igor Támara <igor@tamarapatino.org>
2015-01-11 23:36:48 -0500

    Fixed listing

--- localapi/views.py
+++ localapi/views.py
@@ -39,6 +39,6 @@ class FlisolInstanceRequestList(generics.ListCreateAPIView):
 
     def get_queryset(self):
         queryset = super(FlisolInstanceRequestList, self).get_queryset()
-        if self.request.user.is_superuser == True:
+        if self.request.user.is_staff == True:
             return queryset
         queryset = queryset.filter(created_by=self.request.user)
